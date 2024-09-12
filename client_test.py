07ddae24d88246ac08d6076f2db516b23a6d7753
kdotmanoj <hsuper681@gmail.com>
2024-09-12 14:58:24 +0530

    Added & run testcases

--- client_test.py
+++ client_test.py
@@ -8,6 +8,13 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      expected_price = (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2
+      self.assertEqual(stock, quote['stock'])
+      self.assertEqual(bid_price, float(quote['top_bid']['price']))
+      self.assertEqual(ask_price, float(quote['top_ask']['price']))
+      self.assertAlmostEqual(price, expected_price, places=2)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,6 +22,13 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      expected_price = (float(quote['top_bid']['price']) + float(quote['top_ask']['price'])) / 2
+      self.assertEqual(stock, quote['stock'])
+      self.assertEqual(bid_price, float(quote['top_bid']['price']))
+      self.assertEqual(ask_price, float(quote['top_ask']['price']))
+      self.assertAlmostEqual(price, expected_price, places=2)
 
 
   """ ------------ Add more unit tests ------------ """
